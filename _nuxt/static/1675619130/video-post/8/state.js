93a87acb9620de491b999493c2080c6b2bcb358c
code-dexterous <code-dexterous@users.noreply.github.com>
2023-02-05 17:56:49 +0000

    deploy: 38a7b68276dbef5567ece56050cedcefbf6e2bb2

--- _nuxt/static/1675619130/video-post/8/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675619130",layout:"default",error:a,state:{counter:0,videos:{playlists:[],featuredVideos:[],trendings:[],latests:[],playerVideo:a}},serverRendered:true,routePath:"\u002Fvideo-post\u002F8",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
