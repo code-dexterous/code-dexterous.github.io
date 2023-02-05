af50fc4fe66f1d318b91f4f3689d0dde21c17579
CodeDexterous <codedexterous@gmail.com>
2023-02-05 17:21:18 +0530

    add meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
     base: '/',
   },
   head: {
-    title: 'codedexterous',
+    title: 'CodeDexterous',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Code Dexterous' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
